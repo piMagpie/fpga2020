fb2ce59facef8183ab7b2e72541e54bcaa7da93f
dvillafruela <agazorsuroot@gmail.com>
2020-01-20 10:11:11 +0200

    matrix multiplication exercise

--- matrixMultiplication/matrixMultiplication.sim/sim_1/behav/xsim/xsim.dir/parallel_multiplayer_tb_behav/webtalk/xsim_webtalk.tcl
+++ matrixMultiplication/matrixMultiplication.sim/sim_1/behav/xsim/xsim.dir/parallel_multiplayer_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir C:/fpga/matrixMultiplication/matrixMultiplication.sim/sim_1/behav/xsim/xsim.dir/parallel_multiplayer_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Mon Jan 20 09:47:12 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Jan 20 10:10:51 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "95803ea1-a127-4423-8517-c1531cbcd0ad" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "c37393974c9448bf9e19c7ebb41c203d" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "34" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "35" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-4712MQ CPU @ 2.30GHz" -context "user_environment"
@@ -27,6 +27,6 @@ webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "5620_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1240924193 -regid "" -xml C:/fpga/matrixMultiplication/matrixMultiplication.sim/sim_1/behav/xsim/xsim.dir/parallel_multiplayer_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/fpga/matrixMultiplication/matrixMultiplication.sim/sim_1/behav/xsim/xsim.dir/parallel_multiplayer_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/fpga/matrixMultiplication/matrixMultiplication.sim/sim_1/behav/xsim/xsim.dir/parallel_multiplayer_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Memory -value "5624_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 622966892 -regid "" -xml C:/fpga/matrixMultiplication/matrixMultiplication.sim/sim_1/behav/xsim/xsim.dir/parallel_multiplayer_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/fpga/matrixMultiplication/matrixMultiplication.sim/sim_1/behav/xsim/xsim.dir/parallel_multiplayer_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/fpga/matrixMultiplication/matrixMultiplication.sim/sim_1/behav/xsim/xsim.dir/parallel_multiplayer_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
